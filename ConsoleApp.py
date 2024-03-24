45535f1cf71ae075728c76ba862b1e1d777db6eb
Balalaika73 <97967443+Balalaika73@users.noreply.github.com>
2024-03-24 22:18:19 +0300

    Update ConsoleApp.py

--- ConsoleApp.py
+++ ConsoleApp.py
@@ -1,6 +1,5 @@
-# get var from console
-user_input = input("Write var: ")
+# получить значение с консоли
+int user_input = input("Введите число: ")
 user_input1 = input("Write var: ")
 
-# print var
-print("You wrote:", user_input)
+print("Вы написали:", user_input)
