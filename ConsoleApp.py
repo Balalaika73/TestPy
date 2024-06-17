9aa81dec3c258a5766a357b63c68dc5fee5021a9
Balalaika73 <97967443+Balalaika73@users.noreply.github.com>
2024-06-17 09:55:59 +0300

    Update ConsoleApp.py

--- ConsoleApp.py
+++ ConsoleApp.py
@@ -1,12 +1,9 @@
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 # Вводим переменные с клавиатуры
 name = input("Введите ваше имя: ")
 age = int(input("Введите ваш возраст: "))
 city = input("Введите ваш город проживания: ")
 
 # Выводим информацию
-print("Привет, меня зовут", name)
-print("Мне", age, "лет")
-print("Я живу в городе", city)
+print("Hi, i'm", name)
+print("I'm", age, "years old")
+print("I live in", city)
