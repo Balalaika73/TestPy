1b0cb4d0a2ab63f8a5dd7b73dc33213329baf8e9
Balalaika73 <97967443+Balalaika73@users.noreply.github.com>
2024-06-23 20:14:15 +0300

    Update ConsoleApp.py

--- ConsoleApp.py
+++ ConsoleApp.py
@@ -5,22 +5,22 @@ class Person:
     
     def greet(self):
         if self.age >= 18:
-            return f"Привет, {self.name}! Вы совершеннолетний."
+            return f"Hello, {self.name}! You are an adult."
         else:
-            return f"Привет, {self.name}! Вы несовершеннолетний."
+            return f"Hello, {self.name}! You are not an adult."
 
 def main():
-    name = input("Введите ваше имя: ")
-    age = int(input("Введите ваш возраст: "))
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
     
     person = Person(name, age)
     print(person.greet())
 
     for i in range(age):
         if i % 2 == 0:
-            print(f"{i} - четное число")
+            print(f"{i} - even number")
         else:
-            print(f"{i} - нечетное число")
+            print(f"{i} - odd number")
 
 if __name__ == "__main__":
     main()
